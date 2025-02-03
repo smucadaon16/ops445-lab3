3facfdbf943946f4b0f638a7bee6822bac9e5349
Sheila Cadaon <smcadaon@gmail.com>
2025-02-02 20:58:33 -0500

    lab3b.py

--- lab3a.py
+++ lab3a.py
@@ -25,8 +25,3 @@ if __name__ == '__main__':
     print(text)
     number = return_number_value()
     print(str(number))
-
-import lab3a
-text = lab3a.return_text_value()
-print(text)
-print(lab3a.return_number_value())
